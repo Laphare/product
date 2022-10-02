a8df9f93f79e87aba2a05e5bdfe357ea8b8e00c4
Laphare <laphareqi@gmial.com>
2022-10-02 15:31:18 +0100

    add check file

--- product.py
+++ product.py
@@ -1,4 +1,23 @@
+import os
 product = []
+
+# 检查
+if os.path.isfile('products.csv'):
+	print ('file exist')
+
+	with open ('products.csv', 'r') as f:
+		for line in f:
+			if '商品,价格' in line:
+				continue
+			name, price = line.strip().split(',')
+			product. append([name, price])
+		
+	print (product)
+else:
+	Print('file does not exist')
+
+
+# 写入
 while True:
 	name = input('请输入商品名称：')
 	if name == 'q':
@@ -9,3 +28,10 @@ while True:
 print ('name','price')
 for p in product:
 	print (p[0], p[1])
+
+# 更新
+
+with open ('products.csv', 'w') as f:
+	f.write('商品,价格\n')
+	for p in product:
+		f.write (p[0] + ','+ p[1]+ '\n')
